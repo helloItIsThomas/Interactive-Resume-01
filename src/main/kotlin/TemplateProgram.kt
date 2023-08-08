de1aa01e22ee6142dd501b0c312212b6b940fb73
Thomas McElmeel <81656327+helloItIsThomas@users.noreply.github.com>
2023-08-08 13:03:03 -0400

    remove shader stuff

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -31,32 +31,10 @@ fun main() = application {
         Global.frameCount = frameCount
         val animation = Animation()
         val myFontSize = 100.0
-        val glyph = 'a'
         val myFontMap = loadFont("data/fonts/default.otf", myFontSize)
-        val myFontMapTwo: FontImageMap = FontImageMap.fromFile("data/fonts/default.otf",
-            myFontSize, setOf('a', 'b')
-        )
-//        myFontMap.texture.flipV = true
-        myFontMapTwo.texture.flipV = true
-
-        val aGlyphRect: IntRectangle? = myFontMapTwo.map['a']
-        val aGlyphSize: Vector2 = Vector2(aGlyphRect!!.width.toDouble(), aGlyphRect.height.toDouble())
-        val aTexturePos: Vector2 = Vector2(aGlyphRect.x.toDouble(), aGlyphRect.y.toDouble())
-
-        // an attempt at normalizing texture sizes
-        val aTextureSize = Vector2(
-            myFontMapTwo.texture.width.toDouble(),
-            myFontMapTwo.texture.height.toDouble()
-        )
-        val aTexturePosNormalized = aTexturePos / aTextureSize
-        val aGlyphSizeNormalized = (aGlyphSize / aTextureSize)
-        animation.loadFromJson(File("data/keyframes/keyframes-0.json"))
-        val hypeLowThresh = 0.25
-        val hypeHighThresh = 0.75
-
-
         val MrLine0 = MrLine()
         MrLine0.loadFromJson(File("data/keyframes/keyframes-0.json"))
+        animation.loadFromJson(File("data/keyframes/keyframes-0.json"))
 
         extend {
             Global.frameCount = frameCount
@@ -77,15 +55,6 @@ fun main() = application {
                 MrLine0.check(e)
             }
 
-            val source = Rectangle(
-                aTexturePos.x,
-                aTexturePos.y,
-                aGlyphSize.x,
-                aGlyphSize.y
-            )
-
-            val target = Rectangle(10.0, height - 110.0, myFontSize * 0.5, myFontSize * 0.5)
-
             drawer.fill = null
             drawer.stroke = ColorRGBa.BLACK
             drawer.strokeWeight = 0.75
@@ -101,8 +70,6 @@ fun main() = application {
             drawer.stroke = null
             drawer.circle(myC.position(MrLine0.newPos), 5.0)
 
-            drawer.image(myFontMapTwo.texture, source, target)
-//            drawer.image(myFontMapTwo.texture)
         }
     }
 }
